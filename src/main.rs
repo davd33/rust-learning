91298a53014096180e099034dc968e9e1822d05a
David Rueda <davd33@gmail.com>
2026-02-07 14:10:26 +0100

    enable method snippets with a boolean

--- src/main.rs
+++ src/main.rs
@@ -3,90 +3,124 @@ use std::{sync::{Arc, Mutex}, thread, time::{Duration, Instant}};
 use trpl::Html;
 
 async fn page_title(url: &str) -> (Option<String>, Duration) {
-    let start = Instant::now();
-    let resp_text = trpl::get(url).await.text().await;
-    let duration = start.elapsed();
-    
-    (
-	Html::parse(&resp_text)
-            .select_first("title")
-            .map(|title| title.inner_html()),
-	duration
-    )
+	let start = Instant::now();
+	let resp_text = trpl::get(url).await.text().await;
+	let duration = start.elapsed();
+
+	(
+		Html::parse(&resp_text)
+			.select_first("title")
+			.map(|title| title.inner_html()),
+		duration
+	)
 }
 
-fn get_faster_response() {
-    trpl::block_on(async {
-	println!("Fastest between Google and davd33.fr?");
-	let title1 = page_title("https://davd33.fr/web/algos");
-	let title2 = page_title("https://www.google.com/");
-
-	let (title, dt) = match trpl::select(title1, title2).await {
-	    trpl::Either::Left(l) => l,
-	    trpl::Either::Right(r) => r,
-	};
-	
-	match title {
-	    Some(t) => {
-		println!("Title = {} | dt = {:?}", t, dt);
-	    },
-	    None => println!("No title!")
+fn get_faster_response(run: bool) {
+	if !run {
+		return;
 	}
-    });
-}
 
-fn count_to_10_threads() {
-    let cnt = Arc::new(Mutex::new(0));
+	println!("GET FASTER RESPONSE");
 
-    let mut handlers = vec![];
-    for i in 0..10 {
-	let t1_cnt = Arc::clone(&cnt);
-	let t1 = thread::spawn(move || {
-	    let mut n = t1_cnt.lock().unwrap();
-	    println!("thread {}: {} + 1", i,  *n);
-	    *n += 1;
+	trpl::block_on(async {
+		println!("Fastest between Google and davd33.fr?");
+		let title1 = page_title("https://davd33.fr/web/algos");
+		let title2 = page_title("https://www.google.com/");
+
+		let (title, dt) = match trpl::select(title1, title2).await {
+			trpl::Either::Left(l) => l,
+			trpl::Either::Right(r) => r,
+		};
+
+		match title {
+			Some(t) => {
+				println!("Title = {} | dt = {:?}", t, dt);
+			},
+			None => println!("No title!")
+		}
 	});
-	handlers.push(t1);
-    }
 
-    for h in handlers {
-	h.join().unwrap();
-    }
-
-    println!("Last result = {}", cnt.lock().unwrap());
+	println!("---");
 }
 
-fn count_to_10_tasks() {
-    trpl::block_on(async {
+fn count_to_10_threads(run: bool) {
+	if !run {
+		return;
+	}
+
+	println!("COUNT TO 10 WITH THREADS");
+
 	let cnt = Arc::new(Mutex::new(0));
 
-	let mut futures = vec![];
+	let mut handlers = vec![];
 	for i in 0..10 {
-	    let t1_cnt = Arc::clone(&cnt);
-	    let t1 = async move {
-		println!("task {} waiting", i);
-		trpl::sleep(Duration::from_millis((10-i)*100)).await;
-		let mut n = t1_cnt.lock().unwrap();
-		println!("task {}: {} + 1", i, *n);
-		*n += 1;
-	    };
-	    futures.push(t1);
+		let t1_cnt = Arc::clone(&cnt);
+		let t1 = thread::spawn(move || {
+			let mut n = t1_cnt.lock().unwrap();
+			println!("thread {}: {} + 1", i,  *n);
+			*n += 1;
+		});
+		handlers.push(t1);
 	}
 
-	trpl::join_all(futures).await;
+	for h in handlers {
+		h.join().unwrap();
+	}
 
 	println!("Last result = {}", cnt.lock().unwrap());
-    });
+	println!("---");
+}
+
+fn count_to_10_tasks(run: bool) {
+	if !run {
+		return;
+	}
+
+	println!("COUNT TO 10 WITH TASKS");
+	trpl::block_on(async {
+		let cnt = Arc::new(Mutex::new(0));
+
+		let mut futures = vec![];
+		for i in 0..10 {
+			let t1_cnt = Arc::clone(&cnt);
+			let t1 = async move {
+				println!("task {} waiting", i);
+				trpl::sleep(Duration::from_millis((10-i)*100)).await;
+				let mut n = t1_cnt.lock().unwrap();
+				println!("task {}: {} + 1", i, *n);
+				*n += 1;
+			};
+			futures.push(t1);
+		}
+
+		trpl::join_all(futures).await;
+
+		println!("Last result = {}", cnt.lock().unwrap());
+	});
+	println!("---");
+}
+
+fn futures_msg_passing(run: bool) {
+	if !run {
+		return;
+	}
+
+	println!("MESSAGE PASSING WITH FUTURES");
+
+	trpl::block_on(async {
+		let (tx, mut rx) = trpl::channel();
+		let val = "hello bro";
+		tx.send(val).unwrap();
+		let received = rx.recv().await.unwrap();
+		println!("received = {}", received);
+	});
+
+	println!("---");
 }
 
 fn main() {
-    println!("GET FASTER RESPONSE");
-    get_faster_response();
-    println!("---");
-    println!("COUNT TO 10 WITH THREADS");
-    count_to_10_threads();
-    println!("---");
-    println!("COUNT TO 10 WITH TASKS");
-    count_to_10_tasks();
-    println!("---");
+	get_faster_response(false);
+	count_to_10_threads(false);
+	count_to_10_tasks(false);
+	futures_msg_passing(true);
 }
